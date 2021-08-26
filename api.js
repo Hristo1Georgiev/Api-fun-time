c50532d922a9ad30f8a1c6866c4aaf0727cc39a4
Hristo Georgiev <hristo1georgiev@yahoo.com>
2021-08-26 13:21:10 +0200

    update picture joke

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
-
+// Declare variables
 
 const apiUrl = "https://icanhazdadjoke.com/";
 const apiPic = "https://icanhazdadjoke.com/j/R7UfaahVfFd.png"
 
+//Function to fetch date from API.
 const getRandomJoke = async () => {
     const result = await fetch(apiUrl, {
         headers: {
@@ -15,17 +16,17 @@ const getRandomJoke = async () => {
 };
 
 
+// Function to fetch the picture joke.
 const fetchPictureJoke = async () => {
     const result = await fetch(apiPic, {
         mode: 'no-cors',
         headers: {
             'accept': 'application/json',
             'User-Agent': 'https://github.com/Hristo1Georgiev'
-
-        },
+        }
     });
-    const picture = await result.json();
+
+    const blob = await result.blob();
+    const picture = URL.createObjectURL(blob);
     return picture;
 };
-
-
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
+// Get Elements
 const jokes = document.querySelector("#jokes");
 const pictureJoke = document.querySelector("#pictureJoke")
 
+//function to add the joke to the dom.
 showJoke = async () => {
     jokes.innerHTML = "";
     const result = await getRandomJoke();
@@ -12,13 +13,19 @@ showJoke = async () => {
 
 randomJoke.addEventListener("click", showJoke);
 
-
+/* 
+Function to add the picture joke to the DOM. 
+I did not manage to get the picture joke to the DOM. 
+I spoke with Ben about it. : Dit is alleen wel buiten 
+de scope van de opdracht,
+dus ik zou het houden bij een fetch van text.
+ */
 getPictureJoke = async () => {
     jokes.innerHTML = "";
     const result = await fetchPictureJoke();
+    console.log(result);
     const newImg = document.createElement("img");
-    newImg.src = "img src=https://icanhazdadjoke.com/j/R7UfaahVfFd.png"
-
+    newImg.src = result
     return jokes.appendChild(newImg);
 }
 pictureJoke.addEventListener("click", getPictureJoke);
